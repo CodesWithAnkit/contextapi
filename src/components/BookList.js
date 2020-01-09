71eef95e9fc028fd772fc8d1e0929ab5fd876103
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-10 02:11:55 +0530

    Intiated a contextAPi app

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Wings of Fire</li>
+          <li>Alchmest</li>
+          <li>5 AM club</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Context API</h1>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
