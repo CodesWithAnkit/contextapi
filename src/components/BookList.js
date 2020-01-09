b75d969b55b44d732cb62fbc611f4ccd728b3bff
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-10 03:14:21 +0530

    Accessing data from COntext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>Wings of Fire</li>
-          <li>Alchmest</li>
-          <li>5 AM club</li>
+          <li style={{ background: theme.ui }}>Wings of Fire</li>
+          <li style={{ background: theme.ui }}>Alchmest</li>
+          <li style={{ background: theme.ui }}>5 AM club</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-function Navbar() {
-  return (
-    <nav>
-      <h1>Context API</h1>
-      <ul>
-        <li>Home</li>
-        <li>Contact</li>
-        <li>About</li>
-      </ul>
-    </nav>
-  );
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
+        <h1>Context API</h1>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
